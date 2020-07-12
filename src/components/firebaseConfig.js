04e42efdece2bdc3d5d67f975674cfc22f31ddb0
AchillesTzav <52657980+AchillesTzav@users.noreply.github.com>
2020-07-13 00:58:35 +0300

    Add files via upload

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyA1_YpA51LhU7M5C201K_G3qbSQpQ47DlM",
+    authDomain: "employee-manager-app-311c8.firebaseapp.com",
+    databaseURL: "https://employee-manager-app-311c8.firebaseio.com",
+    projectId: "employee-manager-app-311c8",
+    storageBucket: "employee-manager-app-311c8.appspot.com",
+    messagingSenderId: "48143151490",
+    appId: "1:48143151490:web:cdd302ae7e807d84265f9b",
+    measurementId: "G-4X2YH8WG1X"
+}
